1676597b9807a153f4cf5ffd1a89d735cba0d1d1
Renzo <renzo@MacBook-Pro-de-Renzo.local>
2020-11-12 11:47:30 -0500

    first commit

---
+++ src/components/ForgeViewer.jsx
@@ -0,0 +1,14 @@
+import React, { useEffect } from 'react'
+import { initializeViewer } from '../utils/viewer.utils.js'
+
+const ForgeViewer = () => {
+  const urn = 'dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6c2FudG9yaW5pX2J1Y2tldC9MYW1iZGFTbWFsbEV4YW1wbGUucnZ0'
+  useEffect(() => {
+    initializeViewer(urn)
+  }, [])
+
+  return (
+    <div id='viewerContainer'></div>
+  )
+}
+export default ForgeViewer;
---
+++ src/utils/viewer.utils.js
@@ -0,0 +1,78 @@
+import axios from 'axios';
+
+/* global Autodesk */
+const url_base = 'http://localhost:5000/'
+
+const getToken = async () => {
+  const {
+    data
+  } = await axios.get(url_base + 'forge/token');
+  return data
+}
+
+export const initializeViewer = async (urn) => {
+  const token = await getToken()
+
+  const viewerOptions = {
+    env: 'AutodeskProduction',
+    accessToken: token,
+    api: 'derivativeV2',
+  };
+
+  var viewerContainer = document.getElementById('viewerContainer')
+  var viewer = new Autodesk.Viewing.Private.GuiViewer3D(viewerContainer, {})
+  window.privateViewer = viewer;
+
+  await Autodesk.Viewing.Initializer(viewerOptions, () => {
+
+    viewer.start();
+
+    Autodesk.Viewing.Document.load(`urn:${urn}`, (doc) => {
+      var defaultModel = doc.getRoot().getDefaultGeometry();
+      viewer.loadDocumentNode(doc, defaultModel);
+    });
+
+  });
+
+  viewer.addEventListener(Autodesk.Viewing.GEOMETRY_LOADED_EVENT, async (view) => {
+    let result = await view.model.getPropertyDb().executeUserFunction(
+      userFunction)
+    window.allElements = result;
+  })
+}
+
+
+function userFunction(pdb) {
+  let _attrIdElemento = -1;
+
+  pdb.enumAttributes(function (i, attrDef, attrRaw) {
+    let name = attrDef.name;
+
+    if (name === 'Elemento') {
+      _attrIdElemento = i;
+      return true;
+    }
+  });
+
+
+  if (_attrIdElemento === -1)
+    return null;
+
+  const found = [];
+  pdb.enumObjects(function (dbId) {
+    pdb.enumObjectProperties(dbId, function (attrId, valId) {
+      if (attrId === _attrIdElemento) {
+        // var value = pdb.getAttrValue(attrId, valId);
+        // if (value === "Viga") {
+        //   found.push(dbId)
+        // }
+
+        found.push(dbId)
+
+        return true;
+      }
+    })
+  });
+
+  return found;
+}
